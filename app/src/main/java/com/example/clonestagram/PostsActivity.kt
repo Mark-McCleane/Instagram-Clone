4faeb669ed4c596eb0ed660a80417c9c30248101
Mark-McCleane <mccleanemark@gmail.com>
2022-07-26 23:46:45 +0100

    Connects to Firestore DB

--- app/src/main/java/com/example/clonestagram/PostsActivity.kt
+++ app/src/main/java/com/example/clonestagram/PostsActivity.kt
@@ -3,13 +3,33 @@ package com.example.clonestagram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
+import java.lang.Exception
 
+private const val TAG = "PostsActivity"
 class PostsActivity : AppCompatActivity() {
+    private lateinit var fireStoreDB: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        // Firestore query
+        val fireStoreDB = FirebaseFirestore.getInstance()
+        val postsReference = fireStoreDB.collection("posts")
+        postsReference.addSnapshotListener{snapshot, exception ->
+            if(exception != null || snapshot == null ){
+                Log.e(TAG, "Exception when querying Firestore Posts")
+                return@addSnapshotListener
+            }
+
+            for(document in snapshot.documents){
+                Log.i(TAG, "Document ${document.id}: ${document.data}")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
